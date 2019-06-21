49d3a0d84d29d86e31012766587c7b4f657d2030
Jason Guan <guanshijie14@gmail.com>
2019-06-21 19:11:58 +0800

    create download project

---
+++ Lighter/Lighter/BencodeUtil.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lighter
+{
+    public class BencodeUtil
+    {
+        public static long Bint(BinaryReader binaryReader)
+        {
+            string number = "";
+            char ch;
+            while ((ch = binaryReader.ReadChar()) != 'e')
+            {
+                number += ch;
+            }
+            return long.Parse(number);
+        }
+
+        public static string Bstring(BinaryReader binaryReader)
+        {
+            string number = "";
+            char ch;
+            while ((ch = binaryReader.ReadChar()) != ':')
+            {
+                number += ch;
+            }
+            byte[] byteStr = binaryReader.ReadBytes(int.Parse(number));
+            return Encoding.UTF8.GetString(byteStr);
+        }
+
+        public static Dictionary<object, object> Bdictionary(BinaryReader binaryReader)
+        {
+            Dictionary<object, object> dic = new Dictionary<object, object>();
+            char ch;
+            int i = 0;
+            object Key = null;
+            while ((ch = (char)binaryReader.PeekChar()) != 'e')
+            {
+                i++;
+                object Value;
+                switch (ch)
+                {
+                    case 'i':
+                        // Integer
+                        binaryReader.ReadChar();
+                        Value = Bint(binaryReader); break;
+                    case 'l':
+                        // List
+                        binaryReader.ReadChar();
+                        Value = Blist(binaryReader); break;
+                    case 'd':
+                        // Dictionary
+                        binaryReader.ReadChar();
+                        Value = Bdictionary(binaryReader); break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        Value = Bstring(binaryReader); break;
+                    default:
+                        Value = null; break;
+                }
+                if (i % 2 == 0)
+                {
+                    dic.Add(Key, Value);
+                }
+                else
+                {
+                    Key = Value;
+                }
+            }
+            binaryReader.ReadChar();
+            return dic;
+        }
+
+        public static List<object> Blist(BinaryReader binaryReader)
+        {
+            List<object> list = new List<object>();
+            char ch;
+            while ((ch = (char)binaryReader.PeekChar()) != 'e')
+            {
+                object Value;
+                switch (ch)
+                {
+                    case 'i':
+                        // Integer
+                        binaryReader.ReadChar();
+                        Value = Bint(binaryReader); break;
+                    case 'l':
+                        // List
+                        binaryReader.ReadChar();
+                        Value = Blist(binaryReader); break;
+                    case 'd':
+                        // Dictionary
+                        binaryReader.ReadChar();
+                        Value = Bdictionary(binaryReader); break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        Value = Bstring(binaryReader); break;
+                    default:
+                        Value = null; break;
+                }
+                list.Add(Value);
+            }
+            binaryReader.ReadChar();
+            return list;
+            
+        }
+    }
+}
---
+++ Lighter/Lighter/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lighter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "torrent文件|*.torrent";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            string filepath = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                filepath = openFileDialog1.FileName;
+            StreamReader sr = new StreamReader(filepath);
+            string s = sr.ReadToEnd();
+            FileStream file = File.OpenRead(filepath);
+            BinaryReader binaryReader = new BinaryReader(file);
+            char next = (char)binaryReader.PeekChar();
+            object fileInfo;
+            switch (next)
+            {
+                case 'i':
+                    // Integer
+                    binaryReader.ReadChar();
+                    fileInfo = BencodeUtil.Bint(binaryReader); break;
+                case 'l':
+                    // List
+                    binaryReader.ReadChar();
+                    fileInfo = BencodeUtil.Blist(binaryReader); break;
+                case 'd':
+                    // Dictionary
+                    binaryReader.ReadChar();
+                    fileInfo = BencodeUtil.Bdictionary(binaryReader); break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    fileInfo = BencodeUtil.Bstring(binaryReader); break;
+                default:
+                    fileInfo = null; break;
+            }
+        }
+    }
+}
